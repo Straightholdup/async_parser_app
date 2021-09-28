ed7ba10c73779a0f5c41e3b5fb84414819658428
unknown <askhat1008@gmail.com>
2021-09-28 15:34:42 +0600

    initial parser

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+from service import async_parser_app
+
+dirname = os.path.dirname(__file__)
+config_dir = os.path.join(dirname, 'config.json')
+
+# TODO CHANGE DEBUG MODE IN config.json TO FALSE TO ENABLE MONITORING
+app=async_parser_app.AsyncParserApp(config_dir=config_dir)
+
+
+async def main():
+    async with app.safe_requests.get("https://www.google.ru/") as resp:
+        print(await resp.text())
+
+
+
+if __name__ == "__main__":
+    app.run(main)
